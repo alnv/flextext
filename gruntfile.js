ce4d77adeed62832b031fbec69311037f7e59c01
Alexander Naumov <naumov@tozen.de>
2014-08-05 13:08:19 +0200

    remove no used  contribs

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,32 @@
 module.exports = function(grunt){
 	
 	grunt.initConfig({
-		concat: {
-			
-		},
+	
 		uglify: {
-			
+			options: {
+				mangle: true,
+				compress: true,
+				sourceMap: 'dest/flextext.map',
+			},
+			target: {
+				src: 'src/flextext.js',
+				dest: 'dest/jquery.flextext.min.js'
+			}
 		},
-		jshint: {
-			
-		},
+	
 		watch: {
-			
+			script: {
+				files: ['src/*.js', 'example/*.html'],
+				tasks: ['uglify'],
+				options: {
+					livereload: 3111
+				}
+			}
 		}
+	
 	});
 	
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	
 	grunt.registerTask("default", ['concat', 'uglify']);
